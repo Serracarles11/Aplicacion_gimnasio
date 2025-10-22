7387b5ed4adcb835486def243769ae4ceeafa59a
Serracarles11 <Carlesserra685@gmail.com>
2025-10-22 21:20:53 +0200

    cambio2

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-// next.config.ts
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  // Si solo despliegas en Vercel, puedes incluso quitarla.
-  // Sirve para desarrollo local, no para producción.
-  allowedDevOrigins: ['http://localhost:3000', 'http://<TU_IP_LOCAL>:3000'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig
