baf50d6c13f388f42d47b9a969a5c9120dfac29a
Serracarles11 <Carlesserra685@gmail.com>
2025-10-22 21:18:16 +0200

    dasdad

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next'
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ['http://localhost:3000', 'http://<TU_IP_LOCAL>:3000'],
-  },
-};
-module.exports = nextConfig;
+const nextConfig: NextConfig = {
+  // Si solo despliegas en Vercel, puedes incluso quitarla.
+  // Sirve para desarrollo local, no para producción.
+  allowedDevOrigins: ['http://localhost:3000', 'http://<TU_IP_LOCAL>:3000'],
+}
+
+export default nextConfig
